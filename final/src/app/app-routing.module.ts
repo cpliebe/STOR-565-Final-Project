a70def3070bd92e5afa0a15b36bb6577cce55233
cpliebe <cpliebe@email.unc.edu>
2023-03-23 16:21:49 -0400

    create new app

--- final/src/app/app-routing.module.ts
+++ final/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProposalComponent } from './proposal/proposal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'proposal', component: ProposalComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
