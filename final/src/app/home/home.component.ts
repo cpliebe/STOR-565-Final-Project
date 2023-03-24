3e9c967cc97cc91e4a8d75ebf04705dabdd6a06a
cpliebe <cpliebe@email.unc.edu>
2023-03-23 21:12:23 -0400

    add display of csv file (not done)

--- final/src/app/home/home.component.ts
+++ final/src/app/home/home.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CsvService } from '../csv.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  nfldata: any[] = [];
+  nflheaders: string[] = [];
+
+  constructor(private csvService: CsvService) {}
+  
+  ngOnInit() {
+    this.getData();
+  }
+
+  getData() {
+    this.csvService.getInfo().subscribe(data => {
+      const list = data.split('\n');
+      list.forEach(e => {
+        this.nfldata.push(e);
+      }
+)
+    })
+  }
+
+  getHeaders() {
+    for(let i = 0; i < 23; i++)
+    {
+      this.nflheaders.push(this.nfldata[i])
+    }
+  }
+
+ 
 }
+
